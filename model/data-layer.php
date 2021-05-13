d013735315f9941fea38050d726ef2e7b209d941
Peter Eang <peang@mail.greenriver.edu>
2021-05-13 05:24:51 -0400

    added multiple files for the controller model example

---
+++ model/data-layer.php
@@ -0,0 +1,25 @@
+<?php
+
+/*  data-layer.php
+ *  Return data for the diner app
+ *
+ */
+
+// Get the meals for the order form
+function getMeals()
+{
+    return array('breakfast', 'lunch', 'dinner');
+}
+
+function getCondiments()
+{
+    return array('ketchup', 'mustard', 'sriracha');
+}
+
+/*
+ * 1. Help each other
+ * 2. Add a getCondiment() function to the Model
+ * 3. Modify your Controller to get the condiments
+ *    from the Model and send them to the View
+ * 4. Modify the View page to display the conds
+ */
---
+++ model/validation.php
@@ -0,0 +1,11 @@
+<?php
+
+/*  validation.php
+ *  Validate data for the diner app
+ *
+ */
+
+function validFood($food)
+{
+    return strlen(trim($food)) >= 2;
+}
